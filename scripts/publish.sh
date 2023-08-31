231ef9590034282ed7a850567c54f641277313eb
yang yang <yang@kupotech.com>
2023-09-01 00:34:47 +0800

    mod

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,8 +3,8 @@
 
 cp -rf /Users/yangyang/data/githubs/uniswap-v3/build/* ./cdn
 cp /Users/yangyang/data/githubs/uniswap-v3/build/index.html ./
-cp /Users/yangyang/data/githubs/uniswap-v3/build/index.html ./swap.html
-cp /Users/yangyang/data/githubs/uniswap-v3/build/index.html ./pools.html
+cp /Users/yangyang/data/githubs/uniswap-v3/build/index.html ./swap/index.html
+cp /Users/yangyang/data/githubs/uniswap-v3/build/index.html ./pools/index.html
 
 git add .
 git commit -m "mod"
